a867c6552a43b882118aed278bcb522132838dc3
Md. Atiour Islam <atiour.islam@gmail.com>
2017-06-09 12:38:17 +0600

    Add project files.

---
+++ YesSir.App/YesSir.App/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using YesSir.App.Helpers;
+
+namespace YesSir.App.ViewModels
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        public MainPageViewModel(INavigationService navigationService)
+                  : base(navigationService)
+        {
+            Title = "Home Page";
+            Settings.Current.PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName == nameof(Settings.UserName))
+                    UserName = Settings.Current.UserName;
+            };
+
+            NavigateCommand = new DelegateCommand<string>(OnNavigateCommandExecuted);
+        }
+
+        private string _userName;
+        public string UserName
+        {
+            get { return _userName; }
+            set { SetProperty(ref _userName, value); }
+        }
+
+        public DelegateCommand<string> NavigateCommand { get; }
+
+        private async void OnNavigateCommandExecuted(string path)
+        {
+            await _navigationService.NavigateAsync(path);
+        }
+
+        public override void OnNavigatedTo(NavigationParameters parameters)
+        {
+            UserName = Settings.Current.UserName;
+        }
+    }
+}
